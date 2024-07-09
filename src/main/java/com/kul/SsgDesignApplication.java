7a2660e03b7d25dc77cc6267a0100a5ec100245a
wushuai <659-4gpe145ln@dingtalk.com>
2024-07-09 19:36:08 +0800

    【项目初始化】

---
+++ src/main/java/com/kul/SsgDesignApplication.java
@@ -0,0 +1,13 @@
+package com.kul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsgDesignApplication.class, args);
+    }
+
+}
